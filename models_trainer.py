bd59f4bd4968017ebde8f52256f3d2b01bf4fcc1
Yakir Hasid <hasidyakir@gmail.com>
2024-07-11 10:37:06 +0300

    Added time format for the folder
    Co-authored-by: Almog Rabani <ralmog98@gmail.com>

--- models_trainer.py
+++ models_trainer.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 import landmarks_annot as lm
+from datetime import datetime
+import os
 
 
 #Prepare Data
@@ -17,19 +19,19 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
 #load the train data as dataframe
-Train_data = pd.read_csv("datasets/landmarks/landmarks_features_train.csv" , header=None)
+Train_data = pd.read_csv("datasets/landmarks/landmarks_relative_480x480_XY_Concat_features_train.csv" , header=None)
 Train_data = Train_data.values #get values
 
 #load the train labels as dataframe
-Train_labels = pd.read_csv("datasets/landmarks/landmarks_labels_train.csv", header=None)
+Train_labels = pd.read_csv("datasets/landmarks/landmarks_relative_480x480_XY_Concat_labels_train.csv", header=None)
 Train_labels = Train_labels.values.ravel() #set the right shape
 
 #load the test data as dataframe and scale the data for normalization
-Test_data = pd.read_csv("datasets/landmarks/landmarks_features_test.csv" , header=None)
+Test_data = pd.read_csv("datasets/landmarks/landmarks_relative_480x480_XY_Concat_features_test.csv" , header=None)
 Test_data = Test_data.values
 
 #load the test labels as dataframe
-Test_labels = pd.read_csv("datasets/landmarks/landmarks_labels_test.csv", header=None)
+Test_labels = pd.read_csv("datasets/landmarks/landmarks_relative_480x480_XY_Concat_labels_test.csv", header=None)
 Test_labels = Test_labels.values.ravel()
 
 #scaler = scaler.fit(np.concatenate((Train_data, Test_data)))
@@ -44,16 +46,17 @@ X_train, X_test, y_train, y_test = Train_data, Test_data, Train_labels, Test_lab
 X_train, y_train = sklearn.utils.shuffle(X_train, y_train, random_state=10)
 
 # focus on specific facial features
-facial_features = lm.feature_selection(lm.ALL)
-X_train = X_train[:,facial_features]
-X_test = X_test[:,facial_features]
+#facial_features = lm.feature_selection(lm.ALL)
+#X_train = X_train[:,facial_features]
+#X_test = X_test[:,facial_features]
 
-model_params = {'KNN': {},
+model_params = {#'KNN': {},
                 'QDA': {},
                 'LDA': {},
                 'GNB': {},
-                'DT': {'random_state': 42},
-                'RF': {'random_state': 42}}
+                'DT': {'max_depth': 9, 'max_features': 136, 'max_leaf_nodes': 370,
+                       'min_samples_leaf': 45, 'min_samples_split': 202,  'random_state': 0}}
+                #'RF': {'random_state': 42}}
 
 eval_result = {}
 fitted_classifiers = {}
@@ -62,17 +65,27 @@ for (model_name, model_params) in model_params.items():
     print(f'Model Parameters: {model_params}')
     clf = Model(model_name, model_params)
     (fitted_classifiers[model_name], eval_result[model_name]) = clf.eval_clf(X_train, X_test, y_train, y_test)
-    
 
-with open('models/classifiers/eval_result.pkl', 'wb') as f:
+
+current_datetime = datetime.today().strftime('%Y%m%d%H%M%S')   
+folder_name =  f'models/classifiers/relative_XY_Concat_{current_datetime}'
+
+if not os.path.exists(folder_name):       
+    os.makedirs(folder_name) 
+    
+os.path.join(folder_name, 'eval_result.pkl')
+with open(os.path.join(folder_name, 'eval_result.pkl'), 'wb') as f:
   # dump information to that file
   pickle.dump(eval_result, f)
 
-with open('models/classifiers/fitted_classifiers.pkl', 'wb') as f:
+df = pd.DataFrame(eval_result)
+df.to_csv(os.path.join(folder_name, 'eval_result.csv'))  
+
+with open(os.path.join(folder_name, 'fitted_classifiers.pkl'), 'wb') as f:
   # dump information to that file
   pickle.dump(fitted_classifiers, f)
 
-with open('models/classifiers/scaler.pkl', 'wb') as f:
+with open(os.path.join(folder_name, 'scaler.pkl'), 'wb') as f:
   # dump information to that file
   pickle.dump(scaler, f) 
 
