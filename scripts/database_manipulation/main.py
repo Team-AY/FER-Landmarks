078130b62263138c234cb31351660fe6ccb7f48d
Yakir Hasid <hasidyakir@gmail.com>
2024-09-03 12:12:16 +0300

    Made a main script that rescales the database and extracts landmarks
    Co-authored-by: Almog Rabani <ralmog98@gmail.com>

---
+++ scripts/database_manipulation/main.py
@@ -0,0 +1,6 @@
+import rescale_database
+import relative_landmarks_extractions_68_480x480
+
+#rescale_database.main()
+relative_landmarks_extractions_68_480x480.main('test')
+relative_landmarks_extractions_68_480x480.main('train')
