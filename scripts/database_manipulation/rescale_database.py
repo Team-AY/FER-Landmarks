60ba8970508bc8efd5dffd0371f1a30805dbfe6e
Yakir Hasid <hasidyakir@gmail.com>
2024-09-03 12:11:56 +0300

    Made the rescale database runnable as an external script
    Co-authored-by: Almog Rabani <ralmog98@gmail.com>

--- scripts/database_manipulation/rescale_database.py
+++ scripts/database_manipulation/rescale_database.py
@@ -1,28 +1,45 @@
 import os
 import cv2
 
-original_database_path = '../../datasets/fer2013/test'
-new_database_path = '../../datasets/fer2013_480x480/test'
+def main():                 
 
-for foldername in os.listdir(original_database_path):
-    original_class_path = os.path.join(original_database_path, foldername)
-    new_class_path = os.path.join(new_database_path, foldername)
-    for filename in os.listdir(original_class_path):
-        img_path = os.path.join(original_class_path, filename)
-        img = cv2.imread(img_path)
-        new_img = cv2.resize(img, [480, 480])   
-        new_img_path = os.path.join(new_class_path, filename)    
-        cv2.imwrite(new_img_path, new_img)
+    original_database_path = 'datasets/fer2013'
+    new_database_path = 'datasets/fer2013_480x480'
 
-original_database_path = '../../datasets/fer2013/train'
-new_database_path = '../../datasets/fer2013_480x480/train'
+    EMOTIONS_LIST = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
-for foldername in os.listdir(original_database_path):
-    original_class_path = os.path.join(original_database_path, foldername)
-    new_class_path = os.path.join(new_database_path, foldername)
-    for filename in os.listdir(original_class_path):
-        img_path = os.path.join(original_class_path, filename)
-        img = cv2.imread(img_path)
-        new_img = cv2.resize(img, [480, 480])   
-        new_img_path = os.path.join(new_class_path, filename)    
-        cv2.imwrite(new_img_path, new_img)        
+    if os.path.isdir(new_database_path):
+        print('Please remove beforehand the old database')
+        exit(0)
+
+    os.mkdir(new_database_path)
+
+    original_db_test = os.path.join(original_database_path, 'test')
+    original_db_train = os.path.join(original_database_path, 'train')
+
+    new_db_test = os.path.join(new_database_path, 'test')
+    new_db_train = os.path.join(new_database_path, 'train')
+
+    os.mkdir(new_db_test)
+    for emotion in EMOTIONS_LIST:
+        os.mkdir(os.path.join(new_db_test, emotion))
+
+    rescale_db(original_db_test, new_db_test)
+
+    os.mkdir(new_db_train)
+    for emotion in EMOTIONS_LIST:
+        os.mkdir(os.path.join(new_db_train, emotion))
+
+    rescale_db(original_db_train, new_db_train)
+
+
+def rescale_db(original_database_path, new_database_path):
+    for foldername in os.listdir(original_database_path):
+        original_class_path = os.path.join(original_database_path, foldername)
+        new_class_path = os.path.join(new_database_path, foldername)
+        for filename in os.listdir(original_class_path):
+            img_path = os.path.join(original_class_path, filename)
+            img = cv2.imread(img_path)
+            new_img = cv2.resize(img, [480, 480])   
+            new_img_path = os.path.join(new_class_path, filename)    
+            cv2.imwrite(new_img_path, new_img)    
