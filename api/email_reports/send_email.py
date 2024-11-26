03119d8c2363f864db1c88aa1cac8cbb83527a93
Yakir Hasid <31670770+YakirHasid@users.noreply.github.com>
2024-11-26 02:22:27 +0200

    Refactor datetime variable name in send_email.py

--- api/email_reports/send_email.py
+++ api/email_reports/send_email.py
@@ -10,9 +10,9 @@ from datetime import datetime
 import os
 
 def send_email_report(filename, current_datetime, most_common_emotion):
-    current_datetime = datetime.strptime(current_datetime, '%Y%m%d%H%M%S').strftime('%d/%m/%Y %H:%M:%S')
+    formatted_datetime = datetime.strptime(current_datetime, '%Y%m%d%H%M%S').strftime('%d/%m/%Y %H:%M:%S')
 
-    subject = f"FER Application - Quick Report - {current_datetime}"
+    subject = f"FER Application - Quick Report - {formatted_datetime}"
     body = f"Hi there!\nHere is the quick report for the video you uploaded.\nThe most common emotion detected is: {most_common_emotion}"
     sender_email = os.getenv("SENDER_EMAIL")
     receiver_email = os.getenv("RECEIVER_EMAIL")
