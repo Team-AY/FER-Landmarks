e55253e342fff50d60ed5710ecdbb114dda375e5
Yakir Hasid <hasidyakir@gmail.com>
2024-11-26 02:09:43 +0200

    fix datetime import

--- api/email_reports/send_email.py
+++ api/email_reports/send_email.py
@@ -5,7 +5,7 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import datetime
+from datetime import datetime
 
 import os
 
