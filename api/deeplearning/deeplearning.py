884322dcd72f5ca591071135768a337195f05624
Yakir Hasid <hasidyakir@gmail.com>
2024-11-30 21:16:58 +0200

    offline report

--- api/deeplearning/deeplearning.py
+++ api/deeplearning/deeplearning.py
@@ -14,6 +14,11 @@ from api.deeplearning.clients.RulClient import RulClient
 
 import dlib
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+
 class DeepLearning_API():
 
     # Load the pre-trained face detector and facial landmark predictor from dlib
@@ -101,6 +106,7 @@ class DeepLearning_API():
         emotions, probs = client.evaluate_model(progress_func=progress_func) 
 
         # TODO: Call report creation here before pop happens  
+        self.full_report(emotions, probs, report=['pai'])
 
         # iterate over faces_array, for each face in face_array, take an emotion and put it in a list, so that emotions is a list of lists
         emotions_array = []
@@ -180,3 +186,73 @@ class DeepLearning_API():
             return RulClient()
         else:                    
             return Poster_V2Client() 
+        
+    def full_report(self, emotions, probs, report=['pai', 'bar', 'time']):
+
+        current_datetime = datetime.today().strftime('%Y%m%d%H%M%S')
+        os.mkdir(f'reports/full_reports/{current_datetime}')
+
+        emotions_df = pd.DataFrame(emotions)
+        filename = f"reports/full_reports/{current_datetime}/full_report.pdf"
+
+        most_common_emotion = emotions_df[0].value_counts().idxmax()
+
+        with matplotlib.backends.backend_pdf.PdfPages(filename) as pdf:    
+            if 'pai' in report:
+                fig = plt.figure(figsize=(12,6))
+                patches, texts, _ = plt.pie(emotions_df[0].value_counts(), labels=emotions_df[0].value_counts().index, autopct='%1.2f%%')
+                percents = 100.*emotions_df[0].value_counts()/emotions_df[0].value_counts().sum()
+                labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(emotions_df[0].value_counts().index, percents)]
+
+                sort_legend = True
+                if sort_legend:
+                    patches, labels, dummy =  zip(*sorted(zip(patches, labels, emotions_df[0].value_counts()),
+                                                        key=lambda x: x[2],
+                                                        reverse=True))
+
+                plt.legend(patches, labels, loc='best', bbox_to_anchor=(-0.1, 1.),
+                        fontsize=8)
+
+                plt.title('Emotion Distribution')
+                plt.show()
+                fig.savefig(f'reports/full_reports/{current_datetime}/full_report_pie_chart.png')
+                pdf.savefig(fig)
+
+            if 'bar' in report:
+                emotion_data = {'emotion': ['happy', 'sad', 'neutral', 'surprise', 'angry', 'fear', 'disgust'],
+                                'amount': []}
+                
+
+                for emotion in emotion_data['emotion']:
+                    if emotion in emotions_df.value_counts():
+                        emotion_data['amount'].append(emotions_df.value_counts()[emotion])
+                    else:
+                        emotion_data['amount'].append(0)
+
+                emotions_df2 = pd.DataFrame(emotion_data)
+                emotions_df2.plot.bar(x='emotion', y='amount', rot=0)        
+                fig = sns.barplot(pd.DataFrame(emotions_df2, columns=['emotion', 'count']), x='emotion', y='count')
+                plt.title('Occurrences of Emotions')
+                #plt.bar(emotions_df['emotion'].value_counts()[0])
+
+                # Display numbers above the bars
+                for index, row in emotions_df2.iterrows():
+                    plt.text(index, row['amount'], row['amount'], color='black', ha="center")                
+                    
+                plt.show()     
+                fig.figure.savefig(f'reports/full_reports/{current_datetime}/full_reports_emotions_occurrences.png')   
+                pdf.savefig(fig.figure)
+
+            if 'time' in report:
+                emotions_df3 = emotions_df[0].map({'happy': 1, 'sad':2, 'neutral':3, 'surprise':4, 'angry':5, 'fear':6, 'disgust':7})
+                y_vals = [1, 2, 3, 4, 5, 6, 7]
+                y_labels = ['happy', 'sad', 'neutral', 'surprise', 'angry', 'fear', 'disgust']
+                fig = plt.figure(figsize=(12,6))
+                plt.plot(emotions_df3, '*')    
+                plt.yticks(y_vals, y_labels)  
+                plt.xlabel('Frame Number')  
+                plt.ylabel('Emotion')
+                plt.title('Emotion Per Frame')
+                plt.show()
+                fig.savefig(f'reports/quick_reports/{current_datetime}/quick_report_emotion_per_frame.png')           
+                pdf.savefig(fig)        
