c279d171ac269f04ff18f6122a31feebbc2eea5b
Yakir Hasid <hasidyakir@gmail.com>
2024-12-13 13:19:40 +0200

    added padding

--- main-gui.py
+++ main-gui.py
@@ -167,7 +167,7 @@ class App(customtkinter.CTk):
         self.camera_display = customtkinter.CTkLabel(self.camera_frame, text="")
         self.camera_display.grid(row=0, column=0)        
 
-        self.description_display = customtkinter.CTkLabel(self.camera_frame, text=self.LIVE_DESCRIPTION_TEXT, font=('Arial', 28), justify="left")
+        self.description_display = customtkinter.CTkLabel(self.camera_frame, text=self.LIVE_DESCRIPTION_TEXT, font=('Arial', 28), justify="left", padx=10, pady=10)
         self.description_display.grid(row=0, column=0)
 
     def tab_offline_init(self):
